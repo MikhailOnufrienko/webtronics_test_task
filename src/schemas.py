77eb2b4a407bb3e74da2e286a88bed663dae9b92
MikhailOnufrienko <ghast13@yandex.ru>
2023-07-04 00:17:50 +0400

    Add schemas.

---
+++ src/schemas.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+from pydantic import constr, EmailStr
+
+
+class UserLogin(BaseModel):
+    login: str
+    password: constr(min_length=8, max_length=50)
+
+
+class UserRegistration(UserLogin):
+    first_name: str | None
+    last_name: str | None
+    email: EmailStr | None
+
+
+class Post(BaseModel):
+    title: constr(max_length=120)
+    content: str
+    creation_dt: datetime
